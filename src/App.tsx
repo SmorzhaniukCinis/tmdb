48d37b63683993ab2fba9d783046ac66d817ca6d
SmorzhaniukCinis <smorzhaniuk.cinis@gmail.com>
2022-03-26 13:18:18 +0200

    creating app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+
+const request = () => {
+  axios.get('https://api.themoviedb.org/3/movie/550?api_key=06738fd5d9c4c2cfaabec88c04400226&language=de').then(res => console.log(res))
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<button onClick={request}>test</button>
     </div>
   );
 }
