cdc18ee47fc85965e4fc7d75dffe333e665998e7
SmorzhaniukCinis <smorzhaniuk.cinis@gmail.com>
2022-03-27 15:40:55 +0300

    api testing
    studying  how API work

--- src/API/authAPI.tsx
+++ src/API/authAPI.tsx
@@ -5,6 +5,12 @@ export const createRequestToken = async () => {
     const response = await instance.get(`authentication/token/new?api_key=${key}` )
     return response.data
 }
+export const createSession = async (token:string) => {
+    const response = await instance.post(`authentication/session/new?api_key=${key}`, {
+        "request_token": token
+    } )
+    return response.data
+}
 
 
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,44 @@
-import React from 'react';
-import axios from "axios";
-import {createRequestToken} from "./API/authAPI";
+import React, {useEffect, useState} from 'react';
+import {createRequestToken, createSession} from "./API/authAPI";
+import {Route, Routes} from "react-router-dom";
 
-const history = useHis  
-
-const request = async () => {
-    const res = await createRequestToken()
-    console.log(res)
-}
 
 function App() {
+
+    const creatingToken = async () => {
+        const res = await createRequestToken()
+        if (res.success) {
+            window.location.replace(
+                `https://www.themoviedb.org/authenticate/${res.request_token}?redirect_to=http://localhost:3000`
+            )
+        }
+    }
+
+    useEffect(() => {
+        const url = new URL(window.location.href)
+        let approved =  url.searchParams.get('approved')
+        let request_token = url.searchParams.get('request_token')
+        if(approved && request_token != null) {
+            createSession(request_token).then(res => console.log(res))
+        }
+    } , [])
+
   return (
     <div>
-<button onClick={request}>test</button>
+        <Routes>
+            <Route path={'/'} element={<Test/>}/>
+        </Routes>
+        <button onClick={creatingToken}>test</button>
     </div>
   );
+}
+
+const Test = () => {
+  return(
+      <div>
+          test
+      </div>
+  )
 }
 
 export default App;
