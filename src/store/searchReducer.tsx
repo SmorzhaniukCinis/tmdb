c1a4496d9c0dc6299c49a4393f20e610b0cc76a6
SmorzhaniukCinis <smorzhaniuk.cinis@gmail.com>
2022-05-10 13:25:13 +0300

    made setting search res in BLL

--- src/store/searchReducer.tsx
+++ src/store/searchReducer.tsx
@@ -2,73 +2,41 @@ import {ActionTypes, RootStateType} from "./store";
 import {createListData, editListData, listType} from "../API/ListAPI/listTypes";
 import {listAPI} from "../API/ListAPI/ListAPI";
 import {Dispatch} from "redux";
+import {MovieType, person, TVType} from "../API/SearchAPI/searchTypes";
+import {searchAPI} from "../API/SearchAPI/searchAPI";
+import {CommonResType} from "../API/accountAPI/accountTypes";
 
-const SET_LIST = 'list/SET_LIST'
-const SET_LOADING = 'list/SET_LOADING'
+const SET_SEARCH_RES = 'search/SET_SEARCH_RES'
+const SET_LOADING = 'search/SET_LOADING'
+
+type result = TVType & MovieType & person
 
 const initialState = {
-    listDetails: null as unknown as listType,
-    isLoading: false
+    isLoading: false,
+    results: null as unknown as CommonResType<result>
 }
 type initialStateType = typeof initialState
 
-export const ListReducer = (state = initialState, action:ActionTypes): initialStateType => {
+export const searchReducer = (state = initialState, action: ActionTypes): initialStateType => {
     switch (action.type) {
-        case SET_LIST:
-            return {...state, listDetails: action.list}
+        case SET_SEARCH_RES:
+            return {...state,}
         case SET_LOADING:
             return {...state, isLoading: action.isLoading}
-        default: return state
+        default:
+            return state
     }
 }
 
-export const listActions = {
-    setList: (list:listType) => ({type: SET_LIST, list} as const),
-    setLoading: (isLoading:boolean) => ({type: SET_LOADING, isLoading} as const),
+export const searchActions = {
+    setSearchRes: (result: CommonResType<result>) => ({type: SET_SEARCH_RES, result} as const),
+    setLoading: (isLoading: boolean) => ({type: SET_LOADING, isLoading} as const),
 }
 
-export const GetList = (id: number) => async (dispatch: Dispatch<ActionTypes>, getState:()=>RootStateType) => {
-    dispatch(listActions.setLoading(true))
-    const list = await listAPI.getList(id)
-    dispatch(listActions.setList(list))
-    dispatch(listActions.setLoading(false))
-}
-export const CreateList = (listDetails:createListData) => async (dispatch: Dispatch<ActionTypes>, getState:()=>RootStateType) => {
-    dispatch(listActions.setLoading(true))
-    const res = await listAPI.createList(listDetails)
-    if (res.success) {
-        dispatch(listActions.setLoading(false))
+export const GetList = (searchString: string, page: number) =>
+    async (dispatch: Dispatch<ActionTypes>, getState: () => RootStateType) => {
+        dispatch(searchActions.setLoading(true))
+        const result = await searchAPI.getMultiSearch(searchString, page)
+        dispatch(searchActions.setSearchRes(result))
+        dispatch(searchActions.setLoading(false))
     }
-}
-export const EditList = (listDetails:editListData, id:number) => async (dispatch: Dispatch<ActionTypes>, getState:()=>RootStateType) => {
-    dispatch(listActions.setLoading(true))
-    const res = await listAPI.editList(listDetails, id)
-    if (res.success) {
-        dispatch(listActions.setLoading(false))
-    }
-}
-export const DeleteList = (id:number) => async (dispatch: Dispatch<ActionTypes>, getState:()=>RootStateType) => {
-    dispatch(listActions.setLoading(true))
-    const res = await listAPI.deleteList(id)
-    if (res.success) {
-        dispatch(listActions.setLoading(false))
-    }
-}
-export const addListItem = (ListId:number, itemId:number) => async (dispatch: Dispatch<ActionTypes>, getState:()=>RootStateType) => {
-    const sessionId = getState().auth.sessionId
-    const res = await listAPI.addListItem(itemId, ListId, sessionId)
-    if (res.status_code === 8) {
-        alert("this item already exist in this list")
-    }
-}
-export const deleteListItem = (listId:number, itemId:number) => async (dispatch: Dispatch<ActionTypes>, getState:()=>RootStateType) => {
-    const sessionId = getState().auth.sessionId
-    const res = await listAPI.deleteListItem(itemId, listId, sessionId)
-    if(res.status_code === 13) {
-        const list = await listAPI.getList(listId)
-        dispatch(listActions.setList(list))
-    } else {
-        alert(res.status_message)
-    }
-}
-
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -3,17 +3,23 @@ import thunk from "redux-thunk";
 import {authActions, AuthReducer} from "./authReducer";
 import {accountActions, AccountReducer} from "./accountReducer";
 import {listActions, ListReducer} from "./listReducer";
+import {searchActions, searchReducer} from "./searchReducer";
 
 
 const rootReducer = combineReducers({
-    auth: AuthReducer ,
-    list: ListReducer ,
-    account:AccountReducer})
+    auth: AuthReducer,
+    list: ListReducer,
+    account: AccountReducer,
+    search: searchReducer,
+})
 export type RootStateType = ReturnType<typeof rootReducer>
 export const store = createStore(rootReducer, applyMiddleware(thunk))
 
 type InferValueType<T> = T extends { [key: string]: infer U } ? U : never
-export type ActionTypes = ReturnType<InferValueType<typeof authActions | typeof accountActions | typeof listActions>>
+export type ActionTypes = ReturnType<InferValueType<typeof authActions
+    | typeof accountActions
+    | typeof listActions
+    | typeof searchActions>>
 
 // @ts-ignore
 window.store = store
