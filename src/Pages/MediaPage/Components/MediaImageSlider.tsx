a33f2f6de2590b9d92cf5136a5affe77177c7f94
SmorzhaniukCinis <smorzhaniuk.cinis@gmail.com>
2022-08-06 12:04:02 +0300

    add imageModal to other places

--- src/Pages/MediaPage/Components/MediaImageSlider.tsx
+++ src/Pages/MediaPage/Components/MediaImageSlider.tsx
@@ -3,17 +3,25 @@ import {direction, images} from "./MediaImage";
 import {imageType} from "../../../API/movieAPI/movieTypes";
 import s from "../mediaPage.module.css";
 import {getImage} from "../../../Common/functions/getImage";
+import {ImageModalWindow} from "../../../Common/Components/ImageModalWindow/ImageModalWindow";
 
 type props = {
     direction: direction
     currentMediaImage: images | undefined
 }
 
-const MediaImageSlider:React.FC<props> = ({direction , currentMediaImage}: props) => {
+const MediaImageSlider: React.FC<props> = ({direction, currentMediaImage}: props) => {
 
     const [currentImages, setCurrentImage] = useState<imageType[]>()
+    const [open, setOpen] = React.useState(false);
+    const [imageForModal, setImageForModal] = React.useState<string>('');
 
-    
+    const handleOpen = (img: string) => {
+        setOpen(true)
+        setImageForModal(img)
+    }
+    const handleClose = () => setOpen(false);
+
     useEffect(() => {
         switch (direction) {
             case 'backdrops': {
@@ -28,10 +36,11 @@ const MediaImageSlider:React.FC<props> = ({direction , currentMediaImage}: props
                 setCurrentImage(currentMediaImage?.logos)
                 break
             }
-            default: break
+            default:
+                break
         }
-    },[currentMediaImage?.backdrops, currentMediaImage?.logos, currentMediaImage?.posters, direction])
-    
+    }, [currentMediaImage?.backdrops, currentMediaImage?.logos, currentMediaImage?.posters, direction])
+
 
     return (
         <div className={s.mediaImageSlider}>
@@ -39,10 +48,12 @@ const MediaImageSlider:React.FC<props> = ({direction , currentMediaImage}: props
                 .slice(0, 10)
                 .map((img, index) =>
                     <img className={s.mediaImageItem}
+                         onClick={() => handleOpen(img.file_path)}
                          key={img.file_path}
                          src={getImage('original', img.file_path)}
                          alt="mediaPhoto"/>
                 )}
+            <ImageModalWindow handleClose={handleClose} open={open} url={imageForModal}/>
         </div>
     );
 };
--- src/Pages/PersonPhotos/Components/PersonImageList.tsx
+++ src/Pages/PersonPhotos/Components/PersonImageList.tsx
@@ -4,14 +4,25 @@ import ImageListItem from '@mui/material/ImageListItem';
 import {images} from "../../../API/PersoneAPI/PersonTypes";
 import {getImage} from "../../../Common/functions/getImage";
 import {Box, CircularProgress} from "@mui/material";
+import {ImageModalWindow} from "../../../Common/Components/ImageModalWindow/ImageModalWindow";
 
 type props = {
     images: images[]
 }
 
 export const PersonImageList: React.FC<props> = ({images}: props) => {
+
+    const [open, setOpen] = React.useState(false);
+    const [imageForModal, setImageForModal] = React.useState<string>('');
+
+    const handleOpen = (img: string) => {
+        setOpen(true)
+        setImageForModal(img)
+    }
+    const handleClose = () => setOpen(false);
+
     return (
-        <ImageList sx={{width: '100%'}} cols={4} >
+        <ImageList sx={{width: '100%'}} cols={4}>
             {!images ?
                 <Box sx={{display: 'flex'}}>
                     <CircularProgress/>
@@ -19,6 +30,7 @@ export const PersonImageList: React.FC<props> = ({images}: props) => {
                 : images?.map((item) => (
                     <ImageListItem key={item.file_path}>
                         <img
+                            onClick={() => handleOpen(item.file_path)}
                             src={`${getImage('original', item.file_path)}?w=164&h=164&fit=crop&auto=format`}
                             srcSet={`${getImage('original', item.file_path)}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
                             alt={'error'}
@@ -26,6 +38,7 @@ export const PersonImageList: React.FC<props> = ({images}: props) => {
                         />
                     </ImageListItem>
                 ))}
+            <ImageModalWindow open={open} handleClose={handleClose} url={imageForModal}/>
         </ImageList>
     );
 }
