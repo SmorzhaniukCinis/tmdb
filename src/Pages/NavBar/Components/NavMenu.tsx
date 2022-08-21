5db72ca878feb9e6af466d777de2c5c57d7dbef7
SmorzhaniukCinis <smorzhaniuk.cinis@gmail.com>
2022-08-21 14:47:26 +0300

    Update NavMenu.tsx

--- src/Pages/NavBar/Components/NavMenu.tsx
+++ src/Pages/NavBar/Components/NavMenu.tsx
@@ -1,17 +1,19 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 export const NawMenu = () => {
+
+    const navigate = useNavigate()
+
     return (
         <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
             <Button
+                onClick={()=> navigate('/')}
                 sx={{my: 2, color: 'inherit', display: 'block'}}
             >
-               <Link to={'/tmdb'} style={{color: 'inherit' , textDecoration: 'none'}}>
                    Home
-               </Link>
             </Button>
         </Box>
     )
