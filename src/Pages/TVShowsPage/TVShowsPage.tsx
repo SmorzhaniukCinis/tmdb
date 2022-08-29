ee8e5bd2683dc565f7491eb11a11f0e9626c954c
SmorzhaniukCinis <smorzhaniuk.cinis@gmail.com>
2022-08-29 19:44:21 +0300

    Update TVShowsPage.tsx

--- src/Pages/TVShowsPage/TVShowsPage.tsx
+++ src/Pages/TVShowsPage/TVShowsPage.tsx
@@ -2,20 +2,28 @@ import React, {useEffect} from 'react';
 import {getOnAirTV} from "../../store/Selectors/tvSelectors";
 import {useDispatch, useSelector} from "react-redux";
 import {GetOnTheAirTVShow} from "../../store/TVReducer";
+import {MediaSlider} from "../../Common/Components/MediaSlider/MediaSlider";
+import {getPopularMedia, getTopRatedMedia} from "../../store/mediaReducer";
+import {getPopularMediaSelector, getTopRatedMediaSelector} from "../../store/Selectors/mediaSelectors";
 
 const TvShowsPage = () => {
 
-    const onAirTVShow = useSelector(getOnAirTV)
+    const onAirTVShows = useSelector(getOnAirTV)
+    const {popularTV} = useSelector(getPopularMediaSelector)
+    const {topRatedTV} = useSelector(getTopRatedMediaSelector)
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(GetOnTheAirTVShow())
+        dispatch(getPopularMedia())
+        dispatch(getTopRatedMedia())
     }, [dispatch])
 
-    console.log(onAirTVShow[0]?.name)
     return (
         <div>
-            test 2
+            <MediaSlider title={'On The Air TV Shows'} content={onAirTVShows}/>
+            <MediaSlider title={'Popular TV Shows'} content={popularTV}/>
+            <MediaSlider title={'Top Rated TV Shows'} content={topRatedTV}/>
         </div>
     );
 };
